c8fb235aed899dd85965c86bc0f2b578f0a239c8
hmstea <hmstea@gmail.com>
2024-02-15 14:59:41 +0900

    popup

--- assets/js/front/common.js
+++ assets/js/front/common.js
@@ -3,7 +3,15 @@ $(document).ready(function(){
         history.back();
     });
 
+    $(".header-close").click(function(){
+        history.back();
+    });
+
     $(".login button").click(function(){
         window.location.href="/html/index.html";
-    })
+    });
+
+    $(".header-search").click(function(){
+        window.location.href="/html/list-search.html";
+    });
 })
