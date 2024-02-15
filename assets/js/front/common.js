0310e79b70ac67a9510ae5f6f2bedaa64cfbb703
hmstea <hmstea@gmail.com>
2024-02-15 09:40:38 +0900

    링크추가

---
+++ assets/js/front/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".header-left").click(function(){
+        history.back();
+    });
+
+    $(".login button").click(function(){
+        window.location.href="/html/index.html";
+    })
+})
